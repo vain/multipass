f7da49c7e372bf01f5c24c7930cb1a1ae9fb5895
Peter Hofmann <scm@uninformativ.de>
2017-03-04 15:48:14 +0100

    Tweak font

--- config.def.h
+++ config.def.h
@@ -2,6 +2,6 @@
 /* (Default = 127 = PAUSE key) */
 unsigned int selection_keycode = 127;
 
-char *font_str = "fixed";
+char *font_str = "fixed:pixelsize=12";
 char *bg_str = "#a0a0a0";
 char *fg_str = "#000000";
